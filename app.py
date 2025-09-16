f8e432fdaf26a758faecc6335e77cb267de4ae7e
Craig Fisher <140781290+Firebro51@users.noreply.github.com>
2025-09-16 12:57:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ with ThreadPoolExecutor(max_workers=2) as executor:
 
 time_api_end = time.time()
 
-print(f"APIs take {time_api_end - totaltime:.3f} seconds")
+st.write(f"APIs take {time_api_end - totaltime:.3f} seconds")
 mlb_teams = data['teams']
 
 # print((mlb_teams[1]))
